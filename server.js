73cd464d1f0b89a6f1b141f707f3813ec1838b31
harshnahar28 <103315043+harshnahar28@users.noreply.github.com>
2022-12-14 23:10:37 -0800

    changes reflected

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const dbUrl = 'mongodb+srv://harsh:<password>@cluster0.fcwc2pu.mongodb.net/?retryWrites=true&w=majority'
+const dbUrl = 'mongodb+srv://harsh:harsh@cluster0.fcwc2pu.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express()
 app.use(express.static(__dirname))
